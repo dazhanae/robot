8067123b0f1925f88f5ba8cfc1482cf49e7385b7
Steve Jobs <jones.wiredbraincoffee@gmail.com>
2020-04-26 21:30:52 -0400

    first commit

--- src/myRobots/chillRobot.java
+++ src/myRobots/chillRobot.java
@@ -6,24 +6,33 @@ import robocode.HitByBulletEvent;
 import robocode.Robot;
 import robocode.ScannedRobotEvent;
 
-public class chillRobot extends Robot {
+public class ChillRobot extends Robot {
 	public void run() {
 		while (true) { 
+			//Moves my robot forward 300 pixels
 			ahead(300);
+			//Turns my gun to the right by 360 degrees
 			turnGunRight(360);
-			back(200);
-			turnGunRight(360);
+			// Moves my robot back 100 pixels backward
+			back(100);
+			//Turns my gun to the left by 360 degrees
+			turnGunLeft(360);
 			
 	}
-	}
+	}// Access modifier Return type=null  
 	 public void onScannedRobot(ScannedRobotEvent e) {
-	        fire(10);
+	       	//immediately fires a bullet 
+		 	fire(10);
+	        //Sets the color of my robot to orange
 	        setBodyColor(Color.orange);
-	    }
+	        //Set the color of my bullets to yellow
+	        setBulletColor(Color.YELLOW);
+	    }//Method
 	 public void onHitByBullet(HitByBulletEvent e) {
-	        
+	        //Moves my robot back 10 pixels when hit by a bullet
 	        back(10);
 	    }
+	 
 }
 
 	  
